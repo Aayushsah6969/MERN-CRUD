64256067d48aa332962b79ce6a0e1fe69b03b2b2
Aayush kumar sah <111872525+Aayushsah6969@users.noreply.github.com>
2024-10-09 15:23:07 +0530

    Update server.js
    fixing the cors error

--- backend/server.js
+++ backend/server.js
@@ -25,14 +25,23 @@ const DB_URI = process.env.MONGODB_URI;
 })();
 
 // Middleware to parse JSON requests
+const allowedOrigins = [
+  'http://localhost:5173', // If you're testing locally
+  'https://mern-crud-frontend-co09.onrender.com' // Deployed frontend URL
+];
+
 app.use(cors({
-  origin: '*', // This allows access from ANY origin
-  credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,  // Allows cookies and credentials
   methods: "GET, POST, PUT, DELETE",
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
-
 app.use(express.json());  
 app.use('/text', TextRoute);
 
